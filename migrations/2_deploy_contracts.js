c904fb2cf86fc1a36f0bbebbd6f29cc5a4476d72
MichOchieng <MichOchieng@gmail.com>
2021-05-07 19:52:25 -0700

    First commit

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
-const Migrations = artifacts.require("Migrations");
+const todoList = artifacts.require("todoList");
 
 module.exports = function(deployer) {
-  deployer.deploy(Migrations);
+  deployer.deploy(todoList);
 };
