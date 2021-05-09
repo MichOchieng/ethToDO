04b3aeba0a419717ca6347980fb79252f752200c
MichOchieng <MichOchieng@gmail.com>
2021-05-09 15:13:45 -0700

    Added task struct, constructor, createTask function and task mapping

--- contracts/todoList.sol
+++ contracts/todoList.sol
@@ -3,4 +3,25 @@ pragma solidity >=0.5.0;
 contract todoList {
     // Keep track of tasks in todo list
     uint public numTasks = 0;
+
+    // Task model
+    struct task{
+        uint id;
+        string content;
+        bool completed;
+    }
+    // Blockchain storage
+    mapping(uint => task) public tasks; // PK uint
+
+    constructor() public{
+        createTask("My first task");
+    }
+
+    function createTask(string memory _content) public {
+        // Increase total task count
+        numTasks++;
+        // Creates task from current numTask and passed string
+        tasks[numTasks] = task(numTasks,_content,false);
+    }
+
 }
