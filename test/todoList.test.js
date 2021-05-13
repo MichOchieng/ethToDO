ba124b7e8a8698099730cacd08b31430e16b569e
MichOchieng <MichOchieng@gmail.com>
2021-05-12 21:11:26 -0700

    Added basic tests for todo list

---
+++ test/todoList.test.js
@@ -0,0 +1,28 @@
+const { assert } = require("chai");
+
+const todoList = artifacts.require("./todoList.sol");
+
+contract('todoList', (accounts) => {
+    // Before a test runs get a copy of the todolist on the blockchain
+    before(async () => {
+        this.todoList = await todoList.deployed()
+    })
+
+    it('Deploys successfully', async () => {
+        // Makes sure there is an actual address being used
+        const address = await this.todoList.address
+        assert.notEqual(address,0x0)
+        assert.notEqual(address,'')
+        assert.notEqual(address,null)
+        assert.notEqual(address,undefined)
+    })
+
+    it('Lists tasks', async () => {
+        // Makes sure task count is correct and tasks can be found by count
+        const numTasks = await this.todoList.numTasks()
+        const task = await this.todoList.tasks(numTasks)
+        assert.equal(task.id.toNumber(), numTasks.toNumber())
+        assert.equal(numTasks.toNumber(), 1)
+        assert.equal(task.completed, false)
+    })
+})
